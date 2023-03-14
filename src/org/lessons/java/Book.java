6a480b8ba80e6b2a1b5b2092a8b8d578e1b4fb28
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-14 15:44:24 +0100

    class-book

---
+++ src/org/lessons/java/Book.java
@@ -0,0 +1,71 @@
+package org.lessons.java;
+
+public class Book {
+
+    private String title;
+    private int pages;
+    private String author;
+    private String publisher;
+
+    public Book(String title, int numPages, String author, String publisher) {
+        setTitle(title);
+        setNumPages(numPages);
+        setAuthor(author);
+        setPublisher(publisher);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new IllegalArgumentException("Titolo non valido");
+        }
+        this.title = title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setNumPages(int numPages) {
+        if (numPages <= 0) {
+            throw new IllegalArgumentException("Numero pagine non valido");
+        }
+        this.pages = numPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author == null || author.isEmpty()) {
+            throw new IllegalArgumentException("Autore non valido");
+        }
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        if (publisher == null || publisher.isEmpty()) {
+            throw new IllegalArgumentException("Editore non valido");
+        }
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+
+}
